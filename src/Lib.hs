e6844ac4d9eb3f11dd5290deb355da90871a7cf2
Hussam <Hussam_samir@hotmail.com>
2020-04-12 15:45:19 +0200

    Starts every service now

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
+
+import qualified Data.Foldable as Foldable
+import System.Directory
+import System.Environment
+import System.Process
+
+repos = "./repos"
+
+data Service' =
+  Service
+    { containerId :: String
+    , name :: String
+    }
+
+data DockerService =
+  Service
+    {
+    }
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  args <- getArgs
+  parseArgs args
+
+startServices :: IO ()
+startServices = do
+  fileContent <- readFile repos
+  let repositories = lines fileContent
+  Foldable.forM_ repositories runService
+  putStrLn "Success"
+
+runService :: String -> IO ()
+runService path = do
+  setCurrentDirectory path
+  callCommand "docker-compose up --build -d"
+  putStrLn path
+
+parseArgs :: [String] -> IO ()
+parseArgs ("run":_) = startServices
+parseArgs _ = error "Can not parse the arguments given"
+
+parseServices
